a2bc166d6ffcf66edd30ce9101063b06492e1370
discountry <331027793@qq.com>
2016-08-24 03:36:11 +0000

    finish

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require("express");
+var moment = require("moment");
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var message = `Example usage:<br/>
+  https://timestamp-ms.herokuapp.com/December%2015,%202015<br/>
+https://timestamp-ms.herokuapp.com/1450137600<br/>
+Example output:<br/>
+{ "unix": 1450137600, "natural": "December 15, 2015" }`;
+  res.send(message);
+});
+
+app.get('/:time',function (req,res) {
+    var json = {"unix":moment(req.params.time).format('X'),"natural":moment(req.params.time).format("MMM Do, YY")};
+    res.end(JSON.stringify(json));
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app listening on port 80!');
 });
